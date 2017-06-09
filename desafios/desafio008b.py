f219b8fafbe0b6df2c391b6e91b699ec7c55617d
code36u4r60 <code36u4r60@hotmail.com>
2017-06-09 16:34:44 +0100

    Correção dos exercícios 7 e  8

---
+++ desafios/desafio008b.py
@@ -0,0 +1,18 @@
+#   Desafio 008
+#   Escreva um programa que leia um valor em metros e o exiba convertido em:
+#   Milimetros, Centimetros, Decâmetro, Hectômetro e Quilômetro
+
+medida = float(input('Uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+
+#Decâmetro
+dam = medida / 10
+#Hectômetro
+hm = medida / 100
+#Quilômetro
+km = medida / 1000
+
+print('A media de {}m corresponde a'
+      ' {:.0f}mm, {:.0f}cm, {}dm, {}hm e {}km'
+      .format(medida, mm, cm, dam, hm, km))
