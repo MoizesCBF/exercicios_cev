66275dc1b7dd007993ef7062ad47b5ab9dc1f0b2
code36u4r60 <code36u4r60@hotmail.com>
2017-06-11 02:52:19 +0100

    Add solução ex006

--- desafios/desafio006.py
+++ desafios/desafio006.py
@@ -5,3 +5,11 @@ a = int(input("Número: "))
 print('Dobro de {0} = {1}: '.format(a, a * 2))
 print('Triplo de {0} = {1}: '.format(a, a * 3))
 print('Raiz Quadrada de {0} = {1}: '.format(a, a ** 0.5))
+
+# OU
+
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1 / 2)
+print('O dobro de {0} vale {1}.\nO triplo de {0} vale {2}.\nA raiz quadrada de {0} vale {3:.2f}.'.format(n, d, t, r))
